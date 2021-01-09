68684caf72d2c7cb27b802f175a3d67615b19869
Reshetnikov Artur <cutscenedev@gmail.com>
2021-01-09 13:32:30 +0200

    move App to view layer

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import App from './App';
+import App from './view/App'
+import DependencyContainer from './dependency/dependencyContainer'
+import { DependencyContextProvider } from './dependency/dependencyContext'
+
+const dependencyContainer = new DependencyContainer()
+
+// pre initial-render init
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DependencyContextProvider value={dependencyContainer}>
+      <App />
+    </DependencyContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
---
+++ src/view/App.tsx
@@ -0,0 +1,24 @@
+import { useEffect } from 'react'
+
+import Modaller from './modaller/Modaller'
+import Router from './router/Router'
+import Notifier from './notifier/Notifier'
+import useDependency from '../dependency/useDependency'
+
+function App() {
+  const { } = useDependency()
+
+  useEffect(() => {
+    // post initial-render init
+  }, [])
+
+  return (
+    <div id="app">
+      <Notifier />
+      <Router />
+      <Modaller />
+    </div>
+  )
+}
+
+export default App
